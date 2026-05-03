d0f24f86f3d98957907a0056d9de5f952d1cc7d7
officiallyberno <bernobottcher@gmail.com>
2026-05-03 19:10:34 +0200

    Kleine Testanpassungen

--- src/main/java/tablut/TestMain.java
+++ src/main/java/tablut/TestMain.java
@@ -14,21 +14,23 @@ public class TestMain {
     public static void main(String[] args) {
         Board b = new Board();
 
-           for (int i = 0; i < 11; i++) {
-            for (int j = 0; j < 11; j++) {
-                if (i == 0 || i == 10 || j == 0 || j == 10) {
-                    b.playingBoard[i][j] = Board.BORDER;
-                } else {
-                    b.playingBoard[i][j] = Board.EMPTY;
-                }
-            }
-        }
 
-        String f_1 =  "9/5r3/6rR1/5RK2/6R1r/9/1R8/9/1r7 w 0 33";
-        String f_2 = "4r4/9/9/4R4/3rKr3/4r4/9/9/9 s 0 23";
 
-        perft(f_1);
-        perft(f_2);
+        //    for (int i = 0; i < 11; i++) {
+        //     for (int j = 0; j < 11; j++) {
+        //         if (i == 0 || i == 10 || j == 0 || j == 10) {
+        //             b.playingBoard[i][j] = Board.BORDER;
+        //         } else {
+        //             b.playingBoard[i][j] = Board.EMPTY;
+        //         }
+        //     }
+        // }
+
+        String s_1 = "2s2s3/2w6/9/w2ww3s/s2sKw1ss/s3w4/9/9/5s3 w 0 9";
+
+        perft(s_1);
+        
+
     }
 
     public static void perft(String fen){
--- src/test/java/tablut/tests/BoardTests.java
+++ src/test/java/tablut/tests/BoardTests.java
@@ -78,4 +78,50 @@ void kingIsOnThrone() {
         assertNotEquals(Board.WHITE, board.playingBoard[5][5]);
     }
 
+     // echte Ecken
+    @Test
+    void testIsCorner_TrueCases() {
+        Board b = new Board();
+        b.setUpBoard();
+        assertTrue(b.isCorner(1, 1));
+        assertTrue(b.isCorner(1, 9));
+        assertTrue(b.isCorner(9, 1));
+        assertTrue(b.isCorner(9, 9));
+    }
+
+    // keine Ecken
+    @Test
+    void testIsCorner_FalseCases() {
+        Board b = new Board();
+        b.setUpBoard();
+        assertFalse(b.isCorner(5, 5));
+        assertFalse(b.isCorner(1, 5)); 
+        assertFalse(b.isCorner(5, 1)); 
+        assertFalse(b.isCorner(4, 4)); 
+    }
+        // ✅ Corner + Thron
+    @Test
+    void testIsRestricted_TrueCases() {
+        Board b = new Board();
+        b.setUpBoard();
+        assertTrue(b.isRestricted(1, 1));
+        assertTrue(b.isRestricted(1, 9));
+        assertTrue(b.isRestricted(9, 1));
+        assertTrue(b.isRestricted(9, 9));
+
+        // Thron
+        assertTrue(b.isRestricted(5, 5));
+    }
+
+    // ❌ normale Felder
+    @Test
+    void testIsRestricted_FalseCases() {
+        Board b = new Board();
+        b.setUpBoard();
+        assertFalse(b.isRestricted(5, 4));
+        assertFalse(b.isRestricted(4, 5));
+        assertFalse(b.isRestricted(6, 6));
+        assertFalse(b.isRestricted(2, 2));
+    }
+
 }
